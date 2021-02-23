50ff7027394ffaed31d5672b541db290a03b71c9
Ryan Smith <res33100@gmail.com>
2021-02-22 22:54:06 -0500

    Initial configuration of styled-jsx

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,5 +67,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-jsx`
   ],
 }
--- src/components/common-elements/card/card.js
+++ src/components/common-elements/card/card.js
@@ -1,78 +1,77 @@
 import React from "react"
-import styled from "styled-components"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: var(--black);
-  display: flex;
-`
-
-const StyledContainer = styled.article`
-  flex: 1 1 auto;
-  border-radius: 6px;
-  box-shadow: var(--medium-shadow);
-  font-size: 1rem;
-  color: var(--black);
-  background: var(--white);
-  transition: box-shadow 0.25s ease-in-out;
-
-  &:hover {
-    box-shadow: unset;
-    box-shadow: var(--large-shadow);
-  }
-  
-  &:active {
-    box-shadow: unset;
-    box-shadow: var(--small-shadow);
-  }
-`
-
-const Content = styled.div`
-  padding: 25px;
-
-  & > * {
-    margin: 0 0 15px 0;
-  }
-`
-
-const Heading = styled.h3`
-  color: var(--blue-600);
-  font-size: 1rem;
-  font-weight: 500;
-`
-
-const Timestamp = styled.p`
-  color: var(--gray-600);
-`
-
-const StyledImage = styled(Img)`
-  border-radius: 6px 6px 0 0;
-`
 
 const Card = ({ link, image, imageAltText, heading, timestamp, content }) => {
   return (
     <>
-      <StyledLink to={link} aria-label={heading}>
-        <StyledContainer>
-          <StyledImage
+      <Link
+        to={link}
+        aria-label={heading}
+        style={{
+          textDecoration: "none",
+          color: "var(--black)",
+          display: "flex",
+        }}
+      >
+        <article>
+          <Img
             sizes={{ ...image, aspectRatio: 21 / 9 }}
             alt={imageAltText}
+            style={{
+              borderRadius: "6px 6px 0 0",
+            }}
           />
 
-          <Content>
-            <Timestamp>
-              {timestamp}
-            </Timestamp>
+          <div>
+            <p className="timestamp">{timestamp}</p>
 
-            <Heading>{heading}</Heading>
-
+            <h3>{heading}</h3>
 
             <p>{content}</p>
-          </Content>
-        </StyledContainer>
-      </StyledLink>
+          </div>
+        </article>
+      </Link>
+
+      <style jsx>{`
+        article {
+          flex: 1 1 auto;
+          border-radius: 6px;
+          box-shadow: var(--medium-shadow);
+          font-size: 1rem;
+          color: var(--black);
+          background: var(--white);
+          transition: box-shadow 0.25s ease-in-out;
+        }
+
+        article:hover {
+          box-shadow: unset;
+          box-shadow: var(--large-shadow);
+        }
+
+        article:active {
+          box-shadow: unset;
+          box-shadow: var(--small-shadow);
+        }
+
+        div {
+          padding: 25px;
+        }
+
+        div > * {
+          margin: 0 0 15px 0;
+        }
+
+        h3 {
+          color: var(--blue-600);
+          font-size: 1rem;
+          font-weight: 500;
+        }
+
+        .timestamp {
+          color: var(--gray-600);
+        }
+      `}</style>
     </>
   )
 }
